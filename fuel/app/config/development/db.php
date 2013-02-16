4f93fa81899363b6a9f976b9bc1ec84d77831791
hirose504 <hirose504@yahoo.co.jp>
2013-02-16 16:43:45 +0900

    change db name

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev',
+			'dsn'        => 'mysql:host=localhost;dbname=hackday',
 			'username'   => 'root',
 			'password'   => 'root',
 		),
